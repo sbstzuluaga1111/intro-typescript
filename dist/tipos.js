eeb6434c3c0377ce1a6914a445def6c32eebb143
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-08-14 13:02:39 -0500

    V2

--- dist/tipos.js
+++ dist/tipos.js
@@ -1,4 +1,33 @@
 "use strict";
 console.log('Hola');
 let elBestGroup = 'Artemis';
-elBestGroup = '7';
+elBestGroup = 'V2';
+// TIPOS DE DATOS NATIVOS DE JAVASCRIPT
+//string
+let artemisNombre = 'v2';
+console.log(typeof artemisNombre);
+//number
+let artemisQuantity = 40;
+console.log(typeof artemisQuantity);
+//boolean
+let artemisHired = true;
+console.log(typeof artemisHired);
+let artemisFired = false;
+console.log(typeof artemisFired);
+//null
+//undefined
+let artemisSigned;
+console.log(typeof artemisSigned);
+artemisSigned = true;
+console.log(typeof artemisSigned);
+artemisSigned = 10;
+console.log(typeof artemisSigned);
+//object (array)
+//funtion
+// TIPOS DE DATOS AÑADIDOS TYPESCRIPT - AÑADIDOS
+//any
+//unknown
+//never
+//arrays
+//anums
+// TIPOS DE DATOS INFERIDOS
--- src/tipos.ts
+++ src/tipos.ts
@@ -1,23 +1,41 @@
 console.log('Hola');
 
-
 let elBestGroup = 'Artemis'
 
-
 elBestGroup = 'V2'
-
 
 // TIPOS DE DATOS NATIVOS DE JAVASCRIPT
 
 //string
 
+let artemisNombre:string = 'v2'
+console.log(typeof artemisNombre);
+
 //number
 
+let artemisQuantity:number = 40
+console.log(typeof artemisQuantity);
+
 //boolean
+
+let artemisHired:boolean = true
+console.log(typeof artemisHired);
+
+let artemisFired = false
+console.log(typeof artemisFired);
 
 //null
 
 //undefined
+
+let artemisSigned:any
+console.log(typeof artemisSigned);
+
+artemisSigned = true
+console.log(typeof artemisSigned);
+
+artemisSigned = 10
+console.log(typeof artemisSigned);
 
 //object (array)
 
