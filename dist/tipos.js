98a3b1ebca5fcb421f6f4b02d9b073674ce75fe2
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-08-14 12:46:17 -0500

    V1

---
+++ dist/tipos.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('Hola');
+let elBestGroup = 'Artemis';
+elBestGroup = '7';
---
+++ src/tipos.ts
@@ -0,0 +1,38 @@
+console.log('Hola');
+
+
+let elBestGroup = 'Artemis'
+
+
+elBestGroup = 'V2'
+
+
+// TIPOS DE DATOS NATIVOS DE JAVASCRIPT
+
+//string
+
+//number
+
+//boolean
+
+//null
+
+//undefined
+
+//object (array)
+
+//funtion
+
+// TIPOS DE DATOS AÑADIDOS TYPESCRIPT - AÑADIDOS
+
+//any
+
+//unknown
+
+//never
+
+//arrays
+
+//anums
+
+// TIPOS DE DATOS INFERIDOS
